a71bc2bddf9779f5e996e8a4e7a814e2bc92d0ad
Denis <den.bugaenko99@gmail.com>
2024-01-21 17:20:24 +0500

    add strings lib

---
+++ standard_libs/strings_lib/strings_lib.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+
+func LatinLetters(s string) string {
+	sb := &strings.Builder{}
+
+	for _, r := range s {
+		// проверяет, что руна является латинским символом
+		if unicode.Is(unicode.Latin, r) {
+			sb.WriteRune(r)
+		}
+	}
+
+	return sb.String()
+}
+
+func main() {
+	fmt.Println(strings.Contains("test", "es")) // true
+
+	fmt.Println(strings.Count("test", "t")) // 2
+
+	fmt.Println(strings.Fields("  one    two\tthree four ")) // [one two three four]
+
+	fmt.Println(strings.HasPrefix("test", "te")) // true
+
+	fmt.Println(strings.HasSuffix("test", "st")) // true
+
+	fmt.Println(strings.Index("test", "e")) // 1
+
+	fmt.Println(strings.Join([]string{"hello", "world"}, "-")) // hello-world
+
+	fmt.Println(strings.Repeat("a", 5)) // aaaaa
+
+	fmt.Println(strings.Replace("blanotblanot", "not", "***", -1)) // bla***bla***
+	fmt.Println(strings.Replace("blanotblanot", "", "***", -1))    // ***b***l***a***n***o***t***b***l***a***n***o***t***
+
+	fmt.Println(strings.ReplaceAll("one two three", " ", "_"))
+
+	fmt.Println(strings.Split("a-b-c-d-e", "-")) // [a b c d e]
+
+	fmt.Println(strings.ToLower("TEST")) // test
+
+	fmt.Println(strings.ToUpper("test")) // TEST
+
+	fmt.Println(strings.Trim("tetstet", "te")) // s
+
+	fmt.Println(strings.TrimSpace(" hello ")) // "hello"
+
+	sb := strings.Builder{}
+
+	sb.WriteString("hello")
+	sb.WriteString(" ")
+	sb.WriteString("world")
+
+	fmt.Println(sb.String()) // "hello world"
+
+	fmt.Println(LatinLetters("привет hello")) // hello
+}
