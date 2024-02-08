1b677397f29f56f9c36caa514b1d38a1e1fc22dd
denbug <den.bugaenko99@gmail.com>
2024-02-08 19:54:25 +0500

    add strconv examples

--- standard_libs/strconv_lib/strconv_lib.go
+++ standard_libs/strconv_lib/strconv_lib.go
@@ -21,4 +21,17 @@ func main() {
 	stringWithF := "5.00"
 	intNumber, err = strconv.Atoi(stringWithF)
 	fmt.Println(intNumber, err) // 0, err
+
+	s := "23.23456"
+	// 2 параметр - bitSize - 32 или 64 (32 для float32, 64 для float64)
+	// но нужно понимать, что метод все равно вернет float64
+	floatNumber, err := strconv.ParseFloat(s, 64)
+	fmt.Println(floatNumber, err) // 23.23456 <nil>
+
+	// Конкретный пример для разных bitSize:
+	s = "1.0000000012345678"
+	result32, _ := strconv.ParseFloat(s, 32)
+	result64, _ := strconv.ParseFloat(s, 64)
+	fmt.Println("bitSize32:", result32) // вывод 1 (не уместились)
+	fmt.Println("bitSize64:", result64) // вывод  1.0000000012345678
 }
